c2fd9e3199d2c518f87229ed33995c55818fc31c
AlmostEvil665 <u1196904@utah.edu>
2024-03-12 14:36:34 -0600

    emergency refactor of bingo.py. Converts ALL gets and sets to be constant time complexity

--- bingo.py
+++ bingo.py
@@ -17,16 +17,17 @@ class Tile:
 class Team:
     def __init__(self, name):
         self.name = name
-        self.members = []
+        self.members = {}
         self.points = 0
         self.channel = 0
 
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, team):
         self.name = name
         self.points_gained = 0
         self.gp_gained = 0
+        self.team = team
 
     def __str__(self):
         return self.name
@@ -35,52 +36,36 @@ class Player:
 class Bingo:
     def __init__(self):
         self.teams = {}
-        self.game_tiles = []
+        self.players = {}
+        self.game_tiles = {}
         self.tile_completions = defaultdict(int)
 
     def __str__(self):
         output = "Teams\n"
         for team in self.teams.values():
-            player_info = [f"{player.name} ({player.points_gained} points and {player.gp_gained} gold)" for player in team.members]
+            player_info = [f"{player.name} ({player.points_gained} points and {player.gp_gained} gold)" for player in team.members.values()]
             player_names = ", ".join(player_info)
             output += f"\t{team.name} ({team.points} points): {player_names}\n"
 
         output += "\nTiles\n"
-        for tile in self.game_tiles:
+        for tile in self.game_tiles.values():
             output += f"\t{tile.name}: Worth {tile.points} points {tile.recurrence} times\n"
 
         return output
 
     def get_player(self, player_name):
-        for team in self.teams.values():
-            for member in team.members:
-                if member.name == player_name:
-                    return member
+        return self.players[player_name]
 
     def add_value(self, player_name, value):
         player = self.get_player(player_name)
         player.gp_gained = player.gp_gained + value
-
-    def print(self):
-        print("Teams")
-        for team in self.teams.values():
-            player_names = ", ".join(player.name for player in team.members)
-            print(f"\t{team.name} ({team.points} points): {player_names}")
-
-        print()
-        print("Tiles")
-        for tile in self.game_tiles:
-            print(f"\t{tile.name}: Worth {tile.points} points {tile.recurrence} times")
 
     def set_team_channel(self, team_name, channelID):
         self.teams[team_name].channel = channelID
 
 
     def find_team_by_player(self, player_name):
-        for team in self.teams.values():
-            for player in team.members:
-                if player.name == player_name:
-                    return team
+        return self.players[player_name].team
 
     def new_team(self, name):
         self.teams[name] = Team(name)
@@ -96,43 +81,45 @@ class Bingo:
 
     def add_team_member(self, team_name, player_name):
         team = self.teams[team_name]
-        team.members.append(Player(player_name))
+        player = Player(player_name, team)
+        team.members[player_name] = player
+        self.players[player_name] = player
 
     def remove_team_member(self, team_name, player_name):
-        team = self.teams[team_name]
-        for player in team.members:
-            if player.name == player_name:
-                team.members.remove(player)
+        del self.teams[team_name].members[player_name]
+        del self.players[player_name]
 
     def add_tile(self, name, points, recurrence):
-        self.game_tiles.append(Tile(name, points, recurrence))
+        self.game_tiles[name] = Tile(name, points, recurrence)
 
     def award_tile(self, tile_name, team_name, player_name):
-        for tile in self.game_tiles:
-            if tile.name == tile_name:
-                team = self.teams[team_name]
-                if self.tile_completions[(team, tile)] < int(tile.recurrence):
-                    team.points = team.points + int(tile.points)
-                    self.tile_completions[(team, tile)] = self.tile_completions[(team, tile)] + 1
-                    for member in self.teams[team_name].members:
-                        if member.name == player_name:
-                            member.points_gained = member.points_gained + int(tile.points)
-                            return True
-                break
+        tile = self.game_tiles[tile_name]
+        team = self.teams[team_name]
+        player = self.players[player_name]
 
-    def delete_tile(self, tile_name):
-        for tile in self.game_tiles:
-            if tile.name == tile_name:
-                self.game_tiles.remove(tile)
-                return
+        if self.tile_completions[(team, tile)] < int(tile.recurrence):
+            team.points = team.points + int(tile.points)
+            self.tile_completions[(team, tile)] = self.tile_completions[(team, tile)] + 1
+            player.points_gained = player.points_gained + int(tile.points)
 
-    def is_tile(self, dropname):
-        for tile in self.game_tiles:
-            if tile.name == dropname:
-                return True
+            self.game_tiles[tile_name] = tile
+            self.teams[team_name] = team
+            self.players[player_name] = player
+
+            return True
+
         return False
 
-    def get_tile(self, dropname):
-        for tile in self.game_tiles:
-            if tile.name == dropname:
-                return tile
+
+    def delete_tile(self, tile_name):
+        del self.game_tiles[tile_name]
+
+    def is_tile(self, tile_name):
+        try:
+            var = self.game_tiles[tile_name]
+            return True
+        except:
+            return False
+
+    def get_tile(self, tile_name):
+        return self.game_tiles[tile_name]
